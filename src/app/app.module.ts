04ffe0fff9e54d50b18eda637620e30e6e2308ff
Saba <sabatevdorashvili99@gmail.com>
2023-03-16 16:49:03 +0400

    =header finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideNavComponent
+    SideNavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
